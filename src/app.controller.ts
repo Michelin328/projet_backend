c87fa932a0cb86e31f822766398bd031a5b56c46
Ton Nom <ton.email@example.com>
2026-05-26 12:25:55 +0200

    Fix: exclusion public + intégration des 5 maquettes kiné

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-@Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-}
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
